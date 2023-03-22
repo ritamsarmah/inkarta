10ff4ecaf51291727312f606f3e1d831aebb71ff
Ritam Sarmah <sarmah.ritam@gmail.com>
2023-03-21 19:55:07 -0700

    Add guaranteed new image and bug fixes

--- server/app.py
+++ server/app.py
@@ -1,12 +1,12 @@
 from hashlib import md5
 from io import BytesIO
 from pathlib import Path
-import random
 from PIL import Image, ImageOps
 
 from flask import Flask, abort, request, send_file
 
 import json
+import random
 
 IMAGES_DIR = Path('images/')
 ALLOWED_EXTENSIONS = {'bmp', 'png', 'jpg', 'jpeg', 'tiff', 'tif'}
@@ -17,12 +17,12 @@ app.config['UPLOAD_FOLDER'] = IMAGES_DIR
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000  # Limit uploads to 16 MB
 
 
-''' Database Management '''
+''' Database '''
 
 
-def get_db():
+def get_db() -> dict:
     if not DB_PATH.exists():
-        DB_PATH.write_text("{}")
+        DB_PATH.write_text("{\"images\":{}, \"last_id\":\"\"}")
 
     with DB_PATH.open("r") as f:
         return json.load(f)
@@ -49,10 +49,10 @@ def upload():
     if not (file := request.files.get('image')):
         abort(400, description="Image not provided")
 
-    if not (title := request.args.get('title')):
+    if not (title := request.args.get('title', type=str)):
         abort(400, description="Title not specified")
 
-    if not (artist := request.args.get('artist')):
+    if not (artist := request.args.get('artist', type=str)):
         abort(400, description="Artist not specified")
 
     if not has_valid_extension(file.filename):
@@ -62,22 +62,23 @@ def upload():
     img = Image.open(file)  # type: ignore
     img = img.convert('1', dither=Image.NONE)
 
-    # Create unique filename by hashing title and artist
-    key = md5(f"{title}{artist}".encode()) \
+    # Create unique identifier by hashing title and artist
+    id = md5(f"{title}{artist}".encode()) \
         .hexdigest()[:16]
-    filename = f"{key}.bmp"
+    filename = f"{id}.bmp"
     filepath = IMAGES_DIR / filename
 
     # Check that artwork doesn't already exist
     db = get_db()
-    if key in db:
-        return f"Art with title ({title}) and artist ({artist}) already exists", 400
+    if id in db['images']:
+        abort(
+            400, description="Art with title ({title}) and artist ({artist}) already exists")
 
     # Store info to disk
-    db[key] = {
+    db['images'][id] = {
         'title': title,
         'artist': artist,
-        'path': str(filepath)
+        'file': filename
     }
     set_db(db)
 
@@ -89,13 +90,24 @@ def upload():
 
 @app.route("/delete", methods=['DELETE'])
 def delete():
-    if not (filename := request.args.get('filename')):
-        abort(400, description="Filename not specified")
+    if not (id := request.args.get('id')):
+        abort(400, description="File identifier not specified")
 
-    filepath = Path(IMAGES_DIR / filename)
+    db = get_db()
+    if id not in db:
+        abort(400, description="File identifier not found")
+
+    filepath = IMAGES_DIR / db[id]['file']
     if not filepath.exists():
         abort(
-            400, description=f"Image with filename ({filename}) doesn't exist")
+            400, description=f"Image with doesn't exist for file")
+
+    # Remove image file
+    filepath.unlink()
+
+    # Remove art from database
+    db.pop(id)
+    set_db(db)
 
     return "Image successfully deleted"
 
@@ -106,24 +118,36 @@ def download():
     height = request.args.get('height', type=int)
 
     if width is None:
-        abort(400, "Width not specified")
+        abort(400, "Valid width not specified")
     elif height is None:
-        abort(400, "Height not specified")
+        abort(400, "Valid height not specified")
 
     dimensions = (width, height)
 
     # Get random image path
     db = get_db()
-    if len(db.keys()) == 0:
+    ids = list(db['images'].keys())
+    if len(ids) == 0:
         abort(500, "No artwork was found")
 
-    selection = random.choice(list(db.values()))
+    # Select random, different image if needed
+    selected_id = ids[0]
+    if len(ids) > 1:
+        last_id = db['last_id']
+        while selected_id == last_id:
+            selected_id = random.choice(ids)
+
+    # Update last seen image
+    db['last_id'] = selected_id
+    set_db(db)
 
     # Processes the image to the desired dimensions
-    img = Image.open(selection['path'])
+    artwork = db['images'][selected_id]
+    img = Image.open(IMAGES_DIR / artwork['file'])
     img = ImageOps.contain(img, dimensions)
     img = ImageOps.pad(img, dimensions, color=0xFFFFFF)
 
+    # Send the bitmap image
     img_io = BytesIO()
     img.save(img_io, 'BMP')
     img_io.seek(0)
