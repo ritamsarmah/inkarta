2bedc4c42090a89e71dd53767f768504910dd404
Ritam Sarmah <sarmah.ritam@gmail.com>
2023-04-03 22:20:37 -0700

    Create initial connection code

--- pico/main.cpp
+++ pico/main.cpp
@@ -1,29 +1,30 @@
 // TODO: Use lwip/apps/http_client.h???
+#include <cstdio>
+#include <string>
+
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include "secrets.hpp"
-
-#include <cstdio>
-#include <string>
 
 using namespace std;
 
 // TODO: test is to connect to /next and print the output to stdout
 
 int main() {
-    stdio_init_all(); // TODO: Remove after debugging finished
+    stdio_init_all();
 
     // https://datasheets.raspberrypi.com/picow/connecting-to-the-internet-with-pico-w.pdf
-
-    if (cyw43_arch_init()) {
+    if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
         printf("Failed to initialize\n");
         return 1;
     }
 
-    printf("Connecting to Wi-Fi: %s\n", secret::wifi_ssid);
-    if (cyw43_arch_wifi_connect_timeout_ms(secret::wifi_ssid,
-                                           secret::wifi_password,
-                                           CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to Wi-Fi: %s\n", secret::wifi_ssid.c_str());
+    if (cyw43_arch_wifi_connect_timeout_ms(secret::wifi_ssid.c_str(),
+                                           secret::wifi_password.c_str(),
+                                           CYW43_AUTH_WPA2_AES_PSK, 10000)) {
         printf("Failed to connect to Wi-Fi\n");
         return 1;
     }
