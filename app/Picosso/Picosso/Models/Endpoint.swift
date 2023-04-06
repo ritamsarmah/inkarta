fc7b067bff899555f82ff6491d5213b9118bb09d
Ritam Sarmah <sarmah.ritam@gmail.com>
2023-04-06 09:44:45 -0700

    Update Swift endpoint

--- app/Picosso/Picosso/Models/Endpoint.swift
+++ app/Picosso/Picosso/Models/Endpoint.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum Endpoint: String {
-    private static let baseURL = URL(string: "http://192.168.1.50:5000")!
+    private static let baseURL = URL(string: "http://192.168.1.5:5000")!
     
     case all = "/all"       // Get all data
     case image = "/image"   // Retrieve image for artwork
